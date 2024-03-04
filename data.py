13018c60420cd6ca2a83d9641e96bb23636327f8
mboulayeromien <romienmboulaye7@gmail.com>
2024-03-04 16:17:10 +0100

    creating a class called Data to handle data preprocessing

---
+++ data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+class Data:
+    def load_data(self, load_data):
+        data = pd.read_csv(load_data)
+        return data
+    
+    
+    def prepare_data(load_data):
+        
+    
+    
